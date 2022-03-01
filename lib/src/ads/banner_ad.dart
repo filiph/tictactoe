28ad01f0129e446a163fec4c169dbe895a23468e
Filip Hracek <filip.hracek@gmail.com>
2022-03-01 14:44:26 +0100

    Fix race condition
    The widget called _loadAd() two times in a row at widget creation, once from the didChangeDependencies method and the second time from the build method.

--- lib/src/ads/banner_ad.dart
+++ lib/src/ads/banner_ad.dart
@@ -32,7 +32,7 @@ class _MyBannerAdState extends State<MyBannerAd> {
   static final _log = Logger('MyBannerAd');
 
   BannerAd? _bannerAd;
-  bool _isLoaded = false;
+  _LoadingState _adLoadingState = _LoadingState.initial;
   late Orientation _currentOrientation;
 
   @override
@@ -41,7 +41,7 @@ class _MyBannerAdState extends State<MyBannerAd> {
       builder: (context, orientation) {
         if (_currentOrientation == orientation &&
             _bannerAd != null &&
-            _isLoaded) {
+            _adLoadingState == _LoadingState.loaded) {
           _log.info('We have everything we need. Showing the banner now.');
           return SizedBox(
             width: _bannerAd!.size.width.toDouble(),
@@ -76,12 +76,18 @@ class _MyBannerAdState extends State<MyBannerAd> {
 
   /// Load (another) ad, disposing of the current ad if there is one.
   Future<void> _loadAd() async {
-    _log.info('_loadAd() called. Disposing of _bannerAd now.');
+    _log.info('_loadAd() called.');
+    if (_adLoadingState == _LoadingState.loading ||
+        _adLoadingState == _LoadingState.disposing) {
+      _log.info('An ad is already being loaded or disposed. Aborting.');
+      return;
+    }
+    _adLoadingState = _LoadingState.disposing;
     await _bannerAd?.dispose();
     _log.fine('_bannerAd disposed');
     setState(() {
       _bannerAd = null;
-      _isLoaded = false;
+      _adLoadingState = _LoadingState.loading;
     });
 
     final AnchoredAdaptiveBannerAdSize? size =
@@ -106,12 +112,12 @@ class _MyBannerAdState extends State<MyBannerAd> {
       size: size,
       request: AdRequest(),
       listener: BannerAdListener(onAdLoaded: (Ad ad) {
-        _log.info('Ad loaded: ${ad.responseInfo}');
+        _log.info(() => 'Ad loaded: ${ad.responseInfo}');
         setState(() {
           // When the ad is loaded, get the ad size and use it to set
           // the height of the ad container.
           _bannerAd = ad as BannerAd;
-          _isLoaded = true;
+          _adLoadingState = _LoadingState.loaded;
         });
       }, onAdFailedToLoad: (Ad ad, LoadAdError error) {
         _log.warning('Banner failedToLoad: $error');
@@ -124,4 +130,19 @@ class _MyBannerAdState extends State<MyBannerAd> {
     );
     return _bannerAd!.load();
   }
+}
+
+enum _LoadingState {
+  /// The state before we even start loading anything.
+  initial,
+
+  /// The ad is being loaded at this point.
+  loading,
+
+  /// The previous ad is being disposed of. After that is done, the next
+  /// ad will be loaded.
+  disposing,
+
+  /// An ad has been loaded already.
+  loaded,
 }
