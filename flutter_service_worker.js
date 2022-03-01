ad1ad12e1db9eded48133f699fa8590c286610b0
Filip Hracek <filip.hracek@gmail.com>
2022-03-01 15:09:24 +0100

    Built web
    Branch: main
    Commit: fe736de5d35041c5245cd32fd59a3fb702b3efd6 (dirty)
    
    package:peanut 4.1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "e922e16c088725626f6eb7bbfd177dd6",
-"index.html": "c368a886b13635a061c8885c55065c74",
-"/": "c368a886b13635a061c8885c55065c74",
-"main.dart.js": "1b5526f375bf5c0ac3845e7d3b75504a",
+  "version.json": "0b99c42fa3222e57db5e007a28ec214e",
+"index.html": "23aacf124a03942c15ed3567794e02bb",
+"/": "23aacf124a03942c15ed3567794e02bb",
+"main.dart.js": "3759040f03ac041185fc9737a909bb98",
 "mobile.html": "898e4d7a9d5ee94bec5adfa2431979fc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "c8c45dc2d25e4ad129e26bf51c5315b8",
 "assets/AssetManifest.json": "4fbab0b6e8593ee9c800bbbb35d8bdf3",
-"assets/NOTICES": "c9f6514d0a6dec0d587c595fc657ab8b",
+"assets/NOTICES": "61419021873cfc136306b57a256967f5",
 "assets/FontManifest.json": "202bfb5144bfc3359dc068bcf44cba82",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
