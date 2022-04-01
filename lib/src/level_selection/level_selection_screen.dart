bad0e75e90729cbc42408675bb1941ec3131da25
Filip Hracek <filip.hracek@gmail.com>
2022-04-01 12:40:31 +0200

    Beautify level selection screen.

--- lib/src/level_selection/level_selection_screen.dart
+++ lib/src/level_selection/level_selection_screen.dart
@@ -3,7 +3,6 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:tictactoe/src/achievements/player_progress.dart';
 import 'package:tictactoe/src/audio/audio_system.dart';
-import 'package:tictactoe/src/level_selection/levels.dart';
 import 'package:tictactoe/src/style/delayed_appear.dart';
 import 'package:tictactoe/src/style/palette.dart';
 import 'package:tictactoe/src/style/responsive_screen.dart';
@@ -14,81 +13,51 @@ class LevelSelectionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final playerProgress = context.watch<PlayerProgress>();
     final palette = context.watch<Palette>();
 
     return Scaffold(
       backgroundColor: palette.backgroundLevelSelection,
       body: ResponsiveScreen(
-        squarishMainArea: SingleChildScrollView(
-          child: Wrap(
-            alignment: WrapAlignment.center,
-            runAlignment: WrapAlignment.center,
-            children: [
-              DelayedAppear(
-                ms: ScreenDelays.first,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Center(
-                      child: Text(
-                        'Select level',
-                        style: TextStyle(
-                            fontFamily: 'Permanent Marker', fontSize: 30),
-                      ),
-                    ),
+        squarishMainArea: Column(
+          children: [
+            DelayedAppear(
+              ms: ScreenDelays.first,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Center(
+                  child: Text(
+                    'Select level',
+                    style:
+                        TextStyle(fontFamily: 'Permanent Marker', fontSize: 30),
                   ),
                 ),
               ),
-              for (var i = 0; i < gameLevels.length; i++)
-                ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 30,
-                    maxWidth: 120,
-                  ),
-                  child: AspectRatio(
+            ),
+            SizedBox(height: 50),
+            // This is the grid of numbers.
+            Expanded(
+              child: Center(
+                child: AspectRatio(
                     aspectRatio: 1,
-                    child: DelayedAppear(
-                      ms: ScreenDelays.second + i * 70,
-                      child: RoughButton(
-                        onTap: playerProgress.highestLevelReached >=
-                                gameLevels[i].number - 2
-                            ? () => GoRouter.of(context)
-                                .go('/play/session/${gameLevels[i].number}')
-                            : null,
-                        soundEffect: SfxType.erase,
-                        child: Stack(
-                          children: [
-                            SizedBox.expand(
-                              child: Image.asset(
-                                'assets/images/box.png',
-                                fit: BoxFit.cover,
-                              ),
+                    child: Column(
+                      children: [
+                        for (var y = 0; y < 3; y++)
+                          Expanded(
+                            child: Row(
+                              children: [
+                                for (var x = 0; x < 3; x++)
+                                  AspectRatio(
+                                    aspectRatio: 1,
+                                    child: _LevelButton(y * 3 + x + 1),
+                                  )
+                              ],
                             ),
-                            Center(
-                                child: Text(
-                              '#${gameLevels[i].number}',
-                              style: TextStyle(
-                                color: playerProgress.highestLevelReached >=
-                                        gameLevels[i].number - 1
-                                    ? palette.redPen
-                                    : playerProgress.highestLevelReached ==
-                                            gameLevels[i].number - 2
-                                        ? palette.redPen.withOpacity(0.5)
-                                        : palette.ink.withOpacity(0.5),
-                                fontFamily: 'Permanent Marker',
-                                fontSize: 30,
-                              ),
-                            )),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-            ],
-          ),
+                          )
+                      ],
+                    )),
+              ),
+            ),
+          ],
         ),
         rectangularMenuArea: DelayedAppear(
           ms: ScreenDelays.fourth,
@@ -100,6 +69,49 @@ class LevelSelectionScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _LevelButton extends StatelessWidget {
+  final int number;
+
+  const _LevelButton(this.number, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final playerProgress = context.watch<PlayerProgress>();
+    final palette = context.watch<Palette>();
+
+    /// Level is either one that the player has already bested, on one above.
+    final available = playerProgress.highestLevelReached + 1 >= number;
+
+    /// We allow the player to skip one level.
+    final availableWithSkip = playerProgress.highestLevelReached + 2 >= number;
+
+    return DelayedAppear(
+      ms: ScreenDelays.second + (number - 1) * 70,
+      child: RoughButton(
+          onTap: availableWithSkip
+              ? () => GoRouter.of(context).go('/play/session/$number')
+              : null,
+          soundEffect: SfxType.erase,
+          child: SizedBox.expand(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Image.asset(
+                'assets/images/$number.png',
+                semanticLabel: 'Level $number',
+                fit: BoxFit.cover,
+                color: available
+                    ? palette.redPen
+                    : availableWithSkip
+                        ? Color.alphaBlend(
+                            palette.redPen.withOpacity(0.6), palette.ink)
+                        : palette.ink,
+              ),
+            ),
+          )),
     );
   }
 }
